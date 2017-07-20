eba4b12cdf88b0fe6510fc3cabbf304e1b371715
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2017-07-20 14:56:40 +0300

    WIP: pretty-print tree

--- src/MiniKanrenUtils.ml
+++ src/MiniKanrenUtils.ml
@@ -19,31 +19,32 @@ module TreeLogger =
       val root : StateId.t option ref = ref None
 
       method init id =
-        Printf.printf "%s %s\n" (Listener.string_of_event @@ Listener.Custom "root") (StateId.show id);
+        (* Printf.printf "%s %s\n" (Listener.string_of_event @@ Listener.Custom "root") (StateId.show id); *)
         H.reset tbl;
         root := Some id;
         H.add tbl id @@ make_node (Listener.Custom "root")
 
       (** [on_event e parentId id ] creates new node in the tree *)
       method on_event e pid id =
-        Printf.printf "%s %s %s\n" (Listener.string_of_event e) (StateId.show pid) (StateId.show id);
+        (* Printf.printf "%s %s %s\n" (Listener.string_of_event e) (StateId.show pid) (StateId.show id); *)
         let node = H.add tbl id @@ make_node e in
         let parent = H.find tbl pid in
         H.replace tbl pid { parent with children = id :: parent.children }
 
       method print ?(filter = fun _ -> true) ff =
-        let rec helper ff {event; children} =
+        let rec helper {event; children} =
           let print_child childId =
-            Format.fprintf ff "@;%a" helper @@ H.find tbl childId
+            Format.fprintf ff "@;";
+            helper @@ H.find tbl childId
           in
           let print_children = function
           | [] -> ()
           | children ->
-            Format.fprintf ff "@[<v>";
-            List.iter print_child children;
-            Format.fprintf ff "@]"
+            (* Format.fprintf ff "@[<v 2>"; *)
+            List.iter print_child children
+            (* Format.fprintf ff "@]" *)
           in
-          Format.fprintf ff "@[<v>";
+          Format.fprintf ff "@[<v 2>";
           begin
             if filter event then
             Format.fprintf ff "%s" @@ Listener.string_of_event event
@@ -53,7 +54,7 @@ module TreeLogger =
           Format.fprintf ff "@]"
         in
         match !root with
-        | Some root -> Format.fprintf ff "%a@;" helper @@ H.find tbl root
+        | Some root -> helper @@ H.find tbl root; Format.fprintf ff "@;";
         | None -> ()
     end
   end
