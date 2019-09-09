1dcd138387c6c790956f5e22e4fbff181d9a3262
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2019-09-09 15:58:37 +0300

    camlp5 build

--- config/discover.ml
+++ config/discover.ml
@@ -11,11 +11,11 @@ let () =
       List.map (fun arch -> String.concat Filename.dir_sep [camlp5_dir; arch])
         ["pa_o.cmo"; "pa_op.cmo"; "pr_o.cmo"]
     in
-    let archives =
+    let gt_archives =
       Cfg.Process.run_capture_exn cfg
         "ocamlfind" ["query"; "-pp"; "camlp5"; "-a-format"; "-predicates"; "byte"; "GT,GT.syntax.all"]
     in
     let extract = Cfg.Flags.extract_comma_space_separated_words in
-    Cfg.Flags.write_lines "camlp5-flags"
-      (camlp5_archives @ (extract archives))
+    Cfg.Flags.write_lines "camlp5-flags" camlp5_archives;
+    Cfg.Flags.write_lines "gt-flags" @@ extract gt_archives
   )
